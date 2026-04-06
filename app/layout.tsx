0edd49cbddab46adfafe076f828bb3780e1c6962
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-06 11:35:43 -0600

    Implemented theme and toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,10 @@ import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
 
 const inter = Inter({ subsets: ['latin'] })
-
-console.log('--- Debugging metadataBase ---');
-console.log('process.env.NEXT_PUBLIC_SERVER_URL:', process.env.NEXT_PUBLIC_SERVER_URL);
-console.log('Derived SERVER_URL:', SERVER_URL);
-// You might even try to construct the URL in a try-catch block to see the specific error
-try {
-  new URL(SERVER_URL);
-  console.log('SERVER_URL is a valid URL:', SERVER_URL);
-} catch (e) {
-  console.error('SERVER_URL is NOT a valid URL:', SERVER_URL, e);
-}
-console.log('------------------------------');
 
 export const metadata: Metadata = {
   title: {
@@ -34,11 +23,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", inter.className, "font-sans")}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("antialiased", inter.className)}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
