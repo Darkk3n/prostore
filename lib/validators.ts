7196ce437d899244a03b5b187803f56d56143dd7
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-14 11:57:57 -0600

    Added sign in and out actions

--- lib/validators.ts
+++ lib/validators.ts
@@ -21,3 +21,8 @@ export const insertProductsSchema = z.object({
     price: currency,
     rating: z.number(),
 });
+
+export const signInFormSchema = z.object({
+    email: z.email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+});
