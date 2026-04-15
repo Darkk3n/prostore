bc5f8e8587b2dc61ae9e56571ec7ea460911eeea
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-15 09:33:21 -0600

    Created validation for sign up

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import { signIn, signOut } from '@/auth';
+import prisma from '@/db/prisma';
+import { hashSync } from 'bcrypt-ts-edge';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
 
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
     try {
@@ -30,4 +32,40 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 
 export async function signOutUser() {
     await signOut();
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData) {
+    try {
+        const user = signUpFormSchema.parse({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password'),
+            confirmPassword: formData.get('confirmPassword'),
+        });
+        const plainPassword = user.password;
+        user.password = hashSync(user.password, 10);
+
+        await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password,
+            },
+        });
+
+        await signIn('credentials', {
+            email: user.email,
+            password: plainPassword,
+        });
+
+        return { succes: true, message: 'User registered successfully' };
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+        return {
+            success: false,
+            message: 'User was not registered',
+        };
+    }
 }
