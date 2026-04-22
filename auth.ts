10ff018fb82b2a263a720d8680cba4690974660c
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-22 11:32:38 -0600

    Delete carts when sign in or sign up

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { compareSync } from 'bcrypt-ts-edge';
 import NextAuth, { NextAuthConfig } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { cookies } from 'next/headers';
 import { authConfig } from './auth.config';
 
 export const config: NextAuthConfig = {
@@ -55,14 +56,40 @@ export const config: NextAuthConfig = {
             }
             return session;
         },
-        async jwt({ token, user, trigger, session }: any) {
+        async jwt({ token, user, trigger }: any) {
             if (user) {
+                token.id = user.id;
                 token.role = user.role;
                 if (user.name === 'NO_NAME') {
                     token.name = user.email!.split('@')[0];
                     await prisma.user.update({
                         where: { id: user.id },
                         data: { name: token.name },
+                    });
+                }
+            }
+            if (['signIn', 'signUp'].includes(trigger)) {
+                const cookiesObject = await cookies();
+                const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+                if (!sessionCartId) return;
+
+                const sessionCart = await prisma.cart.findFirst({
+                    where: { sessionCartId },
+                });
+
+                if (!sessionCart) return token;
+
+                if (sessionCart.userId !== user.id) {
+                    // Delete current user cart
+                    await prisma.cart.deleteMany({
+                        where: { userId: user.id },
+                    });
+
+                    // Assign new cart
+                    await prisma.cart.update({
+                        where: { id: sessionCart.id },
+                        data: { userId: user.id },
                     });
                 }
             }
