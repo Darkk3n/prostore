5f67cdb87d51e3a61c65ebec2d1768688a382b5b
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-28 12:37:49 -0600

    Implemented order details page

---
+++ app/(root)/order/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { getOrderById } from '@/lib/actions/order.actions';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
+export const metadata: Metadata = {
+    title: 'Order Details',
+};
+
+const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
+    const { id } = await props.params;
+    const order = await getOrderById(id);
+    if (!order) notFound();
+    return <>Order {order.totalPrice}</>;
+};
+
+export default OrderDetailsPage;
