5c43670e4f2e4c8af114381bc36da9ee49f00a6b
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-28 09:26:39 -0600

    Added schemas and types for order

--- lib/validators.ts
+++ lib/validators.ts
@@ -77,3 +77,24 @@ export const paymentMethodSchema = z
         path: ['type'],
         message: 'Invalid Payment Method',
     });
+
+export const insertOrderSchema = z.object({
+    userId: z.string().min(1, 'User ID is required'),
+    itemsPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    totalPrice: currency,
+    paymentMethod: z
+        .string()
+        .refine((data) => PAYMENT_METHODS.includes(data), { message: 'Invalid Payment Method' }),
+    shippingAddress: shippingAddressSchema,
+});
+
+export const insertOrderItemSchema = z.object({
+    productId: z.string(),
+    slug: z.string(),
+    image: z.string(),
+    name: z.string(),
+    price: currency,
+    qty: z.number(),
+});
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,9 @@
-import { insertProductsSchema, paymentMethodSchema, shippingAddressSchema } from '@/lib/validators';
+import {
+    insertOrderItemSchema,
+    insertProductsSchema,
+    paymentMethodSchema,
+    shippingAddressSchema,
+} from '@/lib/validators';
 import z from 'zod';
 import { cartItemSchema, insertCartSchema } from '../lib/validators';
 
@@ -13,3 +18,16 @@ export type Cart = z.infer<typeof insertCartSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 
 export type PaymentMethod = z.infer<typeof paymentMethodSchema>;
+
+export type Order = z.infer<typeof insertOrderItemSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: boolean;
+    paidAt: Date | null;
+    isDelivered: boolean;
+    deliveredAt: Date | null;
+    orderItems: OrderItem[];
+    user: { name: string; email: string };
+};
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
