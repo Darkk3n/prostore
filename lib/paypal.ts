cb0e1df43fd16fba3f20d22e854c46481d744cb4
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-04-29 13:07:47 -0600

    Added create order function

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -2,7 +2,43 @@ import 'dotenv/config';
 
 const base = process.env.PAYPAYL_API_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+export const paypal = {
+    createOrder: async function createOrder(price: number) {
+        const accessToken = await generateAccesstoken();
+        const url = `${base}/v2/checkout/orders`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({
+                intent: 'CAPTURE',
+                purchase_units: [
+                    {
+                        amount: {
+                            currency_code: 'USD',
+                            value: price,
+                        },
+                    },
+                ],
+            }),
+        });
+        return handleResponse(response);
+    },
+    capturePayment: async function capturePayment(orderId: string) {
+        const accessToken = await generateAccesstoken();
+        const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        return handleResponse(response);
+    },
+};
 
 async function generateAccesstoken() {
     const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
@@ -15,9 +51,13 @@ async function generateAccesstoken() {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     });
+    const jsonData = await handleResponse(response);
+    return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
     if (response.ok) {
-        const jsonData = await response.json();
-        return jsonData.access_token;
+        return response.json();
     } else {
         const errorMessage = await response.text();
         throw new Error(errorMessage);
