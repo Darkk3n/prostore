f32a98d902cac8c851c7cebc9ecc40c90af6149b
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-05-05 11:54:55 -0600

    Implemented products page and action

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,82 @@
+import Pagination from '@/components/shared/pagination';
+import { Button } from '@/components/ui/button';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import { getAllProducts } from '@/lib/actions/product.actions';
+import { formatCurrency, formatId } from '@/lib/utils';
+import Link from 'next/link';
+
+const AdminProductsPage = async (props: {
+    searchParams: Promise<{ page: string; query: string; category: string }>;
+}) => {
+    const searchParams = await props.searchParams;
+    const page = Number(searchParams.page) || 1;
+    const searchText = searchParams.query || '';
+    const searchCategory = searchParams.category || '';
+    const products = await getAllProducts({ query: searchText, page, category: searchCategory });
+
+    return (
+        <div className="space-y-2">
+            <div className="flex-between">
+                <h1 className="h2-bold">Products</h1>
+                <Button
+                    asChild
+                    variant="default"
+                >
+                    <Link href="admin/product/create">Create Product</Link>
+                </Button>
+            </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>ID</TableHead>
+                        <TableHead>NAME</TableHead>
+                        <TableHead className="text-right">PRICE</TableHead>
+                        <TableHead>CATEGORY</TableHead>
+                        <TableHead>STOCK</TableHead>
+                        <TableHead>RATING</TableHead>
+                        <TableHead className="w-25">ACTIONS</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {products.data.map((p) => (
+                        <TableRow key={p.id}>
+                            <TableCell>{formatId(p.id)}</TableCell>
+                            <TableCell>{p.name}</TableCell>
+                            <TableCell className="text-right">
+                                {formatCurrency(p.price.toString())}
+                            </TableCell>
+                            <TableCell>{p.category}</TableCell>
+                            <TableCell>{p.stock}</TableCell>
+                            <TableCell>{p.rating.toString()}</TableCell>
+                            <TableCell className="flex gap-1">
+                                <Button
+                                    asChild
+                                    variant="outline"
+                                    size="sm"
+                                >
+                                    <Link href={`/admin/products/${p.id}`}>Edit</Link>
+                                </Button>
+                                {/* DELETE */}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            {products.totalPages && products.totalPages > 1 && (
+                <Pagination
+                    page={page}
+                    totalPages={products.totalPages}
+                />
+            )}
+        </div>
+    );
+};
+
+export default AdminProductsPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 'use server';
 import prisma from '@/db/prisma';
 import 'dotenv/config';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
@@ -14,4 +14,27 @@ export async function getLatestProducts() {
 export async function getProductBySlug(slug: string) {
     const product = await prisma.product.findFirst({ where: { slug: slug } });
     return product;
+}
+
+type GetAllProductsProps = {
+    query: string;
+    limit?: number;
+    page: number;
+    category?: string;
+};
+
+export async function getAllProducts({
+    query,
+    limit = PAGE_SIZE,
+    page,
+    category,
+}: GetAllProductsProps) {
+    const data = await prisma.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+    });
+
+    const dataCount = await prisma.product.count();
+
+    return { data, totalPages: Math.ceil(dataCount / limit) };
 }
