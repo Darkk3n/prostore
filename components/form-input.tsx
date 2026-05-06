4118aac802513fc4010e83d4350a6b2001bdcc76
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-05-06 13:25:26 -0600

    Adapted form input to allow image uploads

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -1,7 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Image from 'next/image';
+import {
+    Control,
+    Controller,
+    ControllerFieldState,
+    ControllerRenderProps,
+    FieldPath,
+    FieldValues,
+    Path,
+} from 'react-hook-form';
+import { toast } from 'sonner';
+
 import { Field, FieldError, FieldLabel } from '@/components/ui/field'; // Adjust path if necessary
 import { Input, InputProps } from '@/components/ui/input'; // Import InputProps for type safety
-import { Control, Controller, FieldValues, Path } from 'react-hook-form';
+import { UploadButton } from '@/lib/uploadthing';
+
+import { Card, CardContent } from './ui/card';
 import { RadioGroup, RadioGroupItem, RadioGroupProps } from './ui/radio-group';
 import { Textarea } from './ui/textarea';
 
@@ -56,6 +70,111 @@ function FormInput<TFieldValues extends FieldValues>(props: FormInputProps<TFiel
         remainingProps = radioRest;
     }
 
+    const renderInput = (
+        field: ControllerRenderProps<TFieldValues, FieldPath<TFieldValues>>,
+        fieldState: ControllerFieldState,
+    ) => {
+        let inputComponent = null;
+        switch (type) {
+            case 'radio':
+                inputComponent = (
+                    <div>
+                        {' '}
+                        <FieldLabel>{label}</FieldLabel> {/* Label for the entire radio group */}
+                        <RadioGroup
+                            {...field} // field.value and field.onChange will connect to RadioGroup's value and onValueChange
+                            {...(remainingProps as Omit<
+                                RadioGroupProps,
+                                'name' | 'defaultValue' | 'value' | 'onValueChange' | 'onChange'
+                            >)}
+                            id={name as string}
+                            onValueChange={(value) => {
+                                field.onChange(value);
+                                customOnValueChange?.(value);
+                            }}
+                            value={field.value} // Explicitly map RHF value to RadioGroup's value
+                        >
+                            <div className="flex flex-col space-y-2">
+                                {(rest as RadioInputProps<TFieldValues>).options.map((option) => (
+                                    <div
+                                        key={option.value}
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <RadioGroupItem
+                                            value={option.value}
+                                            id={`${name as string}-${option.value}`}
+                                            disabled={option.disabled}
+                                        />
+                                        <FieldLabel htmlFor={`${name as string}-${option.value}`}>
+                                            {option.label}
+                                        </FieldLabel>
+                                    </div>
+                                ))}
+                            </div>
+                        </RadioGroup>
+                    </div>
+                );
+                break;
+            case 'textarea':
+                inputComponent = (
+                    <Textarea
+                        {...field}
+                        {...(rest as any)}
+                        className="resize-none"
+                        rows={5}
+                    />
+                );
+            case 'email':
+            case 'password':
+            case 'number':
+            case 'text':
+                inputComponent = (
+                    <Input
+                        {...field} // Spreads onChange, onBlur, value, and ref
+                        {...(rest as Omit<InputProps, 'name' | 'defaultValue'>)} // Cast rest for type safety
+                        id={name as string}
+                        type={type || 'text'} // Default to 'text' if type is undefined
+                        placeholder={(rest as TextInputProps<TFieldValues>).placeholder || label}
+                        aria-invalid={fieldState.invalid}
+                    />
+                );
+                break;
+            case 'image':
+                const currentImages: string[] = (field.value || []) as string[];
+                inputComponent = (
+                    <Card>
+                        <CardContent className="space-y-2 mt-2 min-h-48">
+                            <div className="flex-start space-x-2">
+                                {currentImages.map((image: string) => (
+                                    <Image
+                                        key={image}
+                                        src={image}
+                                        alt="product image"
+                                        className="w-20 h-20 object-cover object-center rounded-sm"
+                                        width={100}
+                                        height={100}
+                                    />
+                                ))}
+                                <UploadButton
+                                    endpoint="imageUploader"
+                                    onClientUploadComplete={(res: { url: string }[]) => {
+                                        field.onChange([...currentImages, res[0].url]);
+                                    }}
+                                    onUploadError={(error: Error) => {
+                                        toast.error(error.message, {
+                                            position: 'top-right',
+                                        });
+                                    }}
+                                />
+                            </div>
+                        </CardContent>
+                    </Card>
+                );
+                break;
+        }
+        return inputComponent;
+    };
+
     return (
         <div className="flex flex-row md:flex-col gap-5">
             <Controller
@@ -66,85 +185,7 @@ function FormInput<TFieldValues extends FieldValues>(props: FormInputProps<TFiel
                         {type !== 'radio' && ( // Only render FieldLabel directly for non-radio types
                             <FieldLabel htmlFor={name as string}>{label}</FieldLabel>
                         )}
-                        {(() => {
-                            switch (type) {
-                                case 'radio':
-                                    return (
-                                        <div>
-                                            {' '}
-                                            <FieldLabel>{label}</FieldLabel>{' '}
-                                            {/* Label for the entire radio group */}
-                                            <RadioGroup
-                                                {...field} // field.value and field.onChange will connect to RadioGroup's value and onValueChange
-                                                {...(remainingProps as Omit<
-                                                    RadioGroupProps,
-                                                    | 'name'
-                                                    | 'defaultValue'
-                                                    | 'value'
-                                                    | 'onValueChange'
-                                                    | 'onChange'
-                                                >)}
-                                                id={name as string}
-                                                onValueChange={(value) => {
-                                                    field.onChange(value);
-                                                    customOnValueChange?.(value);
-                                                }}
-                                                value={field.value} // Explicitly map RHF value to RadioGroup's value
-                                            >
-                                                <div className="flex flex-col space-y-2">
-                                                    {(
-                                                        rest as RadioInputProps<TFieldValues>
-                                                    ).options.map((option) => (
-                                                        <div
-                                                            key={option.value}
-                                                            className="flex items-center space-x-2"
-                                                        >
-                                                            <RadioGroupItem
-                                                                value={option.value}
-                                                                id={`${name as string}-${option.value}`}
-                                                                disabled={option.disabled}
-                                                            />
-                                                            <FieldLabel
-                                                                htmlFor={`${name as string}-${option.value}`}
-                                                            >
-                                                                {option.label}
-                                                            </FieldLabel>
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                            </RadioGroup>
-                                        </div>
-                                    );
-                                case 'email':
-                                case 'password':
-                                case 'number':
-                                case 'text':
-                                    return (
-                                        <Input
-                                            {...field} // Spreads onChange, onBlur, value, and ref
-                                            {...(rest as Omit<InputProps, 'name' | 'defaultValue'>)} // Cast rest for type safety
-                                            id={name as string}
-                                            type={type || 'text'} // Default to 'text' if type is undefined
-                                            placeholder={
-                                                (rest as TextInputProps<TFieldValues>)
-                                                    .placeholder || label
-                                            }
-                                            aria-invalid={fieldState.invalid}
-                                        />
-                                    );
-                                case 'textarea':
-                                    return (
-                                        <Textarea
-                                            {...field}
-                                            {...(rest as any)}
-                                            className="resize-none"
-                                            placeholder=""
-                                        />
-                                    );
-                                default:
-                                    return null;
-                            }
-                        })()}
+                        {renderInput(field, fieldState)}
 
                         {fieldState.invalid && fieldState.error && (
                             <FieldError>{fieldState.error.message}</FieldError>
