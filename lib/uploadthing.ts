1e86fa41ab9e19387e5904e449b02a461753b6d6
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-05-06 13:26:03 -0600

    Misc

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,6 @@
-import { OurFileRouter } from '@/app/uploadthing/core';
 import { generateUploadButton, generateUploadDropzone } from '@uploadthing/react';
+
+import { OurFileRouter } from '@/app/api/uploadthing/core';
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
