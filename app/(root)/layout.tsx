084ea2af96a5a3eb310d42b3c0b771e40683fcd0
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-05-06 13:25:52 -0600

    Added missing import

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,18 @@
 import Footer from '@/components/footer';
 import Header from '@/components/shared/header';
 
+import '@/assets/styles/globals.css';
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <main className="flex-1 wrapper">{children}</main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex h-screen flex-col">
+            <Header />
+            <main className="flex-1 wrapper">{children}</main>
+            <Footer />
+        </div>
+    );
 }
