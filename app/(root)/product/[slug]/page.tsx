a253db55171200901dd448c5ae31bcf339dff775
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-05-12 15:54:19 -0600

    Created review list

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from '@/auth';
 import AddToCart from '@/components/shared/product/add-to-cart';
 import ProductImages from '@/components/shared/product/product-images';
 import ProductPrice from '@/components/shared/product/product-price';
@@ -6,6 +7,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { getMyCart } from '@/lib/actions/cart.actions';
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { notFound } from 'next/navigation';
+import ReviewList from './review-list';
 
 const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
     const { slug } = await props.params;
@@ -13,6 +15,8 @@ const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) 
     if (!product) notFound();
 
     const cart = await getMyCart();
+    const session = await auth();
+    const userId = session?.user.id;
 
     return (
         <>
@@ -77,6 +81,14 @@ const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) 
                         </Card>
                     </div>
                 </div>
+            </section>
+            <section className="mt-10">
+                <h2 className="h2-bold">Customer Reviews</h2>
+                <ReviewList
+                    userId={userId || ''}
+                    productId={product.id}
+                    productSlug={product.slug}
+                />
             </section>
         </>
     );
