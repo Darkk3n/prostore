f4177e7d2220c8fbf4b0b4535ead13f0d00ffe04
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-05-13 11:26:11 -0600

    Added missing props

--- types/index.ts
+++ types/index.ts
@@ -13,6 +13,9 @@ import { cartItemSchema, insertCartSchema } from '../lib/validators';
 
 export type Product = z.infer<typeof insertProductsSchema> & {
     id: string;
+    rating: string;
+    numReviews: number;
+    createdAt: Date;
 };
 
 export type CartItem = z.infer<typeof cartItemSchema>;
