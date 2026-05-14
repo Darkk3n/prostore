a717a13b147a5b2b4d5fcc0898d7f5423b8912a8
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-05-14 10:16:04 -0600

    Added deal countdown

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import DealCountdown from '@/components/deal-countdown';
 import IconBoxes from '@/components/icon-boxes';
 import ProductCarousel from '@/components/shared/product/product-carousel';
 import ProductList from '@/components/shared/product/product-list';
@@ -29,6 +30,7 @@ const HomePage = async () => {
                 limit={4}
             />
             <ViewAllProductsButton />
+            <DealCountdown />
             <IconBoxes />
         </>
     );
